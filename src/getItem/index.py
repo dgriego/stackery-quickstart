ca5f96696470d803a66e65920df272dc7a03a368
dgriego <eggomygriego@gmail.com>
2019-07-27 00:05:40 -0700

    update getItem function

--- src/getItem/index.py
+++ src/getItem/index.py
@@ -3,6 +3,6 @@ import json
 
 def handler(event, context):
     # Log the event argument for debugging and for use in local development.
-    print(json.dumps(event))
+    print(json.parse(event.body))
 
     return {}
